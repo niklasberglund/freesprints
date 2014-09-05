76c813fbeffc8080d1b83a2db344cd1396c4c377
niklasberglund <niklas@asdasd.net>
2014-09-05 17:54:22 +0800

    rootPath() and pluginsPath() helpers

--- source/freesprints/helpers.py
+++ source/freesprints/helpers.py
@@ -1,7 +1,17 @@
 import os
+import os.path
 
 def isRunningOnRPi():
     if os.uname()[4][:3] == 'arm':
         return True
     else:
         return False
+
+
+def rootPath():
+    return os.getcwd()
+    #return os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    # os.path.dirname(os.path.realpath(__file__))
+
+def pluginsPath():
+    return os.path.abspath(os.path.join(rootPath(), "plugins"))
