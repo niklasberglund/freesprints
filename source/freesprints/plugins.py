e4cb178207c375c2790eb0f9c4ffbee41b946b93
niklasberglund <niklas@asdasd.net>
2014-09-05 17:56:01 +0800

    plugins support

--- source/freesprints/plugins.py
+++ source/freesprints/plugins.py
@@ -1,13 +1,70 @@
+import helpers
+import os
+import os.path
+import json
+import imp
 
 class PluginLoader:
-    def __init__():
-        pass
-        
-class VisualisationPlugin:
-    module = None
+    availablePlugins = None
     
-    def __init__():
+    def __init__(self):
+        self.checkAvailablePlugins()
+        
+    def checkAvailablePlugins(self):
+        print helpers.pluginsPath()
+        
+        pluginDirs = [pluginPath for pluginPath in os.listdir(helpers.pluginsPath())] #if os.path.isdir(f)]
+        
+        self.availablePlugins = []
+        
+        for pluginPath in pluginDirs:
+            print pluginPath
+            pluginPathAbsolute = os.path.join(helpers.pluginsPath(), pluginPath)
+            jsonInfoPath = os.path.join(pluginPathAbsolute, "info.json")
+            
+            jsonInfoData = open(jsonInfoPath)
+            
+            jsonInfo = json.load(jsonInfoData)
+            
+            self.availablePlugins.append(Plugin(jsonInfo, pluginPathAbsolute))
+            
+                
+        
+    def getAvailablePlugins(self):
         pass
         
-    def loadModule():
-        pass
+class Plugin:
+    path = None
+    name = None
+    version = None
+    author = None
+    
+    module = None
+    pluginObject = None
+    
+    def __init__(self, infoJson, path):
+        self.path = path
+        
+        self.name = infoJson.get("name")
+        self.version = infoJson.get("version")
+        self.author = infoJson.get("author")
+        
+        print infoJson
+        
+        self.initModule()
+        
+    def initModule(self):
+        #module = imp.find_module("pluginModule", [self.path])
+        self.module = imp.load_source("pluginModule", os.path.join(self.path, "__init__.py"))
+        
+        print "FIND MODULE:"
+        print self.module
+        
+        self.pluginObject = self.module.VisualisationPlugin()
+        self.pluginObject.spinCount(123, 0)
+        
+
+
+
+
+
