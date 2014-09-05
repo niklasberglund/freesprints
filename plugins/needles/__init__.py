c58b49d656feb14dbd5cd5176d79d836f79d6532
niklasberglund <niklas@asdasd.net>
2014-09-05 17:56:16 +0800

    foundation for needles visualisation plugin

---
+++ plugins/needles/__init__.py
@@ -0,0 +1,8 @@
+
+class VisualisationPlugin:
+    def __init__(self):
+        print "init in plugin"
+        
+    def spinCount(self, count, roller):
+        print "spinCount in plugin"
+        
