8fd985eca9eca7271d9e3b7af2135e21db5d6f6d
niklasberglund <niklas@asdasd.net>
2014-09-05 17:55:49 +0800

    updated code for rendering to frame buffer on Raspberry Pi

--- source/freesprints/__init__.py
+++ source/freesprints/__init__.py
@@ -4,14 +4,20 @@ import logging
 from pygame.locals import *
 import FSMenu
 import helpers as helpers
+import plugins
+import os.path
 
 # platform-specific imports
 if (helpers.isRunningOnRPi()): # running on Raspberry Pi
     import RPi.GPIO
     import os
-    os.environ['SDL_VIDEODRIVER']="fbcon"
+    print "ON RASPBERRY PI"
+    #os.environ['SDL_VIDEODRIVER']="fbcon"
+    #os.environ["SDL_FBDEV"] = "/dev/fb1"
+    print "SET DRIVER"
 else: # running on computer
     import FakeRPiGPIO.GPIO
+    
 
 class Application(object):
     # application state constants
@@ -23,11 +29,13 @@ class Application(object):
     menuSurface = None
     menu = None
     state = STATE_MAINMENU
+    pluginLoader = None
     
     def __init__(self):
         print "Application.__init__"
         
         pygame.font.init()
+        pluginLoader = plugins.PluginLoader()
         
         menuOptionsDict = {
             "font_path": "fonts/Cave-Story.ttf",
@@ -77,13 +85,12 @@ class Application(object):
                 "callback": self.exit
             }
         ]
-        self.windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+        #self.windowSurface = pygame.display.set_mode((500, 400), pygame.FULLSCREEN, 32)
+        pygame.display.init()
+        self.windowSurface = pygame.display.set_mode((800, 600), 0, 32)
         
         menuOptions = FSMenu.MenuOptions(menuOptionsDict)
         self.menu = FSMenu.Menu(self.windowSurface, menuStructure, menuOptions)
-        
-    def start(self):
-        print "asd"
         
     def startGame(self):
         print "start game"
@@ -111,6 +118,37 @@ class Application(object):
         # set up pygame
         pygame.init()
         pygame.font.init()
+        
+        
+        if helpers.isRunningOnRPi():
+            disp_no = os.getenv("DISPLAY")
+    
+            if disp_no:
+                print "I'm running under X display = {0}".format(disp_no)
+    
+            # Check which frame buffer drivers are available
+            # Start with fbcon since directfb hangs with composite output
+            drivers = ['fbcon', 'directfb', 'svgalib']
+            found = False
+            for driver in drivers:
+                # Make sure that SDL_VIDEODRIVER is set
+                if not os.getenv('SDL_VIDEODRIVER'):
+                    os.putenv('SDL_VIDEODRIVER', driver)
+                try:
+                    pygame.display.init()
+                except pygame.error:
+                    print 'Driver: {0} failed.'.format(driver)
+                    continue
+                found = True
+                break
+    
+            if not found:
+                raise Exception('No suitable video driver found!')
+                
+            size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
+            print "Framebuffer size: %d x %d" % (size[0], size[1])
+            #self.windowSurface = pygame.display.set_mode(size, pygame.FULLSCREEN)
+                
 
         # set up the window
         pygame.display.set_caption('Freesprints')
@@ -155,9 +193,9 @@ class Application(object):
         pygame.draw.rect(self.windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
 
         # get a pixel array of the surface
-        pixArray = pygame.PixelArray(self.windowSurface)
-        pixArray[480][380] = BLACK
-        del pixArray
+        #pixArray = pygame.PixelArray(self.windowSurface)
+        #pixArray[480][380] = BLACK
+        #del pixArray
 
         # draw the text onto the surface
         self.windowSurface.blit(text, textRect)
@@ -182,7 +220,6 @@ app = None
 
 def init():
     global app
-    
     app = Application()
     
 def start():
