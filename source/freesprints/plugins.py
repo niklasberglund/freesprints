0e4e7f750ccda71ab9d971e797f5c158a0c88ee0
niklasberglund <niklas@asdasd.net>
2014-09-11 01:50:36 +0800

    camelCase => underscores in plugins.py

--- source/freesprints/plugins.py
+++ source/freesprints/plugins.py
@@ -7,7 +7,7 @@ import source.freesprints
 from pygame.locals import *
 
 class PluginLoader:
-    availablePlugins = None
+    available_plugins = None
     
     def __init__(self):
         self.checkAvailablePlugins()
@@ -15,25 +15,25 @@ class PluginLoader:
     def checkAvailablePlugins(self):
         print helpers.pluginsPath()
         
-        pluginDirs = [pluginPath for pluginPath in os.listdir(helpers.pluginsPath())] #if os.path.isdir(f)]
+        plugin_dirs = [plugin_path for plugin_path in os.listdir(helpers.pluginsPath())] #if os.path.isdir(f)]
         
-        self.availablePlugins = []
+        self.available_plugins = []
         
-        for pluginPath in pluginDirs:
-            print pluginPath
-            pluginPathAbsolute = os.path.join(helpers.pluginsPath(), pluginPath)
-            jsonInfoPath = os.path.join(pluginPathAbsolute, "info.json")
+        for plugin_path in plugin_dirs:
+            print plugin_path
+            plugin_path_absolute = os.path.join(helpers.pluginsPath(), plugin_path)
+            json_info_path = os.path.join(plugin_path_absolute, "info.json")
             
-            jsonInfoData = open(jsonInfoPath)
+            json_info_data = open(json_info_path)
             
-            jsonInfo = json.load(jsonInfoData)
+            jsonInfo = json.load(json_info_data)
             
-            self.availablePlugins.append(Plugin(jsonInfo, pluginPathAbsolute))
+            self.available_plugins.append(Plugin(jsonInfo, plugin_path_absolute))
             
                 
         
     def getAvailablePlugins(self):
-        return self.availablePlugins
+        return self.available_plugins
         
 class Plugin:
     path = None
@@ -42,33 +42,33 @@ class Plugin:
     author = None
     
     module = None
-    pluginObject = None
+    plugin_object = None
     
-    def __init__(self, infoJson, path):
+    def __init__(self, info_json, path):
         self.path = path
         
-        self.name = infoJson.get("name")
-        self.version = infoJson.get("version")
-        self.author = infoJson.get("author")
+        self.name = info_json.get("name")
+        self.version = info_json.get("version")
+        self.author = info_json.get("author")
         
-        print infoJson
+        print info_json
         
-        self.initModule()
+        self.init_module()
         
-    def initModule(self):
+    def init_module(self):
         #module = imp.find_module("pluginModule", [self.path])
         self.module = imp.load_source("pluginModule", os.path.join(self.path, "__init__.py"))
         
         print "FIND MODULE:"
         print self.module
 
-        self.pluginObject = self.module.VisualisationPlugin(source.freesprints.get_app())
-        #self.pluginObject.start()
-        #self.pluginObject.spinCount(123, 0)
+        self.plugin_object = self.module.VisualisationPlugin(source.freesprints.get_app())
+        #self.plugin_object.start()
+        #self.plugin_object.spinCount(123, 0)
 
     def start(self):
         source.freesprints.get_app().get_window_surface().fill(Color("black"))
-        self.pluginObject.start()
+        self.plugin_object.start()
 
 
 
