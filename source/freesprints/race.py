1c571f9c75a822367c3359df79afafe31961b4de
niklasberglund <niklas@asdasd.net>
2014-09-16 18:10:47 +0800

    support for participants in Options

--- source/freesprints/race.py
+++ source/freesprints/race.py
@@ -1,9 +1,12 @@
+from pygame.locals import *
 
 class Race(object):
+    options = None
     participants = None
     
-    def __init__(self):
-        pass
+    def __init__(self, options_object):
+        self.options = options_object
+        self.participants = participants_list
 
     def start(self):
         pass
@@ -13,8 +16,22 @@ class Race(object):
 
 
 class Options(object):
+    distance = None # meters
+    participants = None
+    
+    _roller_controller = None
+    
     def __init__(self):
-        pass
+        _roller_controller = RollerController()
+    
+    def set_distance(self, distance):
+        self.distance = distance
+    
+    def set_participants(self, participants):
+        self.participants = participants
+    
+    def get_participants(self):
+        return self.participants
 
 
 class Participant(object):
@@ -22,12 +39,12 @@ class Participant(object):
     name = None
     color = None
     
-    def __init__(self, roller, name = "", color = None):
-        pass
+    def __init__(self, name, roller_pin, color = Color("pink")):
+        self.roller = Roller(roller_pin)
 
 
 class Roller(object):
-    def __init__(self):
+    def __init__(self, pin_number):
         pass
 
 
