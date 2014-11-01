9a70cb972404c203e6a5e6a1eda147a7bf1a4dfe
niklasberglund <niklas@asdasd.net>
2014-11-01 14:40:08 +0800

    functions for sending fake ticks

--- lib/FakeRPiGPIO/FakeRPi/GPIO.py
+++ lib/FakeRPiGPIO/FakeRPi/GPIO.py
@@ -1,5 +1,7 @@
 # http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio
 
+import threading
+
 VERSION = "0.5.2" # because RPi.GPIO 0.5.2 have support for interrupts(including threaded)
 
 INPUT = 1
@@ -25,7 +27,7 @@ SPI = 41
 I2C = 42
 PWM = 43
 
-event_detect_mapping = {}
+event_detect_mapping = []
 
     
 def setmode(mode):
@@ -46,7 +48,25 @@ def wait_for_edge(number, edge):
 def add_event_detect(number, risingFalling, callback):
     global event_detect_mapping
     
-    event_detect_mapping[number] = callback
+    event_detect_mapping[number] = {'callback': callback, 'fake_edge': risingFalling}
 
 def cleanup():
     pass
+
+def start_fake_ticks(number, interval):
+    fake_ticks_timer = threading.Timer(interval, send_fake_tick, [number])
+    event_detect_mapping[number]['timer'] = fake_ticks_timer
+    fake_ticks_timer.start()
+
+def stop_fake_ticks(number):
+    event_detect_mapping[number]['timer'].cancel()
+    event_detect_mapping[number]['timer'] = None
+
+def send_fake_tick(number):
+    global event_detect_mapping
+    
+    event_detect_mapping[number]['callback']()
+
+def fake_edge(number, high_low):
+    pass
+
